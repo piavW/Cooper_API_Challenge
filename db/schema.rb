30fa904af1dd7f893715bd8b1f189eedd5c3fb05
piavw <piavonwachenfelt@hotmail.com>
2019-10-16 10:14:00 +0200

    Creates & configures Performance data model - Generates performance data model - Adds associations between user model and performance data model - Adds spec-tests to check association and database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_074212) do
+ActiveRecord::Schema.define(version: 2019_10_16_080932) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
+
+  create_table "performance_data", force: :cascade do |t|
+    t.bigint "user_id"
+    t.hstore "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_performance_data_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 2019_10_16_074212) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "performance_data", "users"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,4 +52,8 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe 'Relations' do
+    it { is_expected.to have_many :performance_data }
+  end
 end
