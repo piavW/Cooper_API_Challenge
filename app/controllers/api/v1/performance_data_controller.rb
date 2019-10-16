b3b1d96dba8190af57929225338c6f54934138af
piavw <piavonwachenfelt@hotmail.com>
2019-10-16 10:46:15 +0200

    Updates controller to whitelist params.

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  def create
+    @data = PerformanceData.new(performance_data_params)
+    if @data.save
+      render json: { message: 'all good' }
+    else 
+      render json: { error: @data.errors.full_messages }
+    end
+  end
+
+  private
+  def performance_data_params
+    params.require(:performance_data).permit!
+  end
 end
