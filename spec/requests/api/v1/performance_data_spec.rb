90d906ea516f698a02264af95bb79f5d6a77ceef
miyesier <miyesier@gmail.com>
2019-10-16 10:32:34 +0200

    Adds tests and configures performance data - Creates test-file & spec-test for performance-data - Creates controller for performance-data - Configures routes for performance data for api v1

---
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Api::V1::PerformanceDataController, type: :request do
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+  describe 'POST /api/v1/performance_data' do
+    it 'creates a data entry' do
+      post '/api/v1/performance_data', params: {
+        performance_data: { data: { message: 'Average' } }
+      }, headers: headers
+
+      entry = PerformanceData.last
+      expect(entry.data).to eq 'message' => 'Average'
+    end
+  end
+end
