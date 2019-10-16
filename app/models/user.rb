b8138c0366c6d8bd8d15bdca08a1ce97b74e2069
piavw <piavonwachenfelt@hotmail.com>
2019-10-16 15:16:58 +0200

    Reformats files for improved readability.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 class User < ActiveRecord::Base
   extend Devise::Models
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
